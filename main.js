a4ffe201b6071946a2ef31f19b7cf62f7b509896
Anton <Krasikov-94@bk.ru>
2022-11-28 14:46:58 +0300

    pomodoro final

---
+++ main.js
@@ -0,0 +1,8 @@
+// const first = document.getElementById('first-block');
+// first.scrollIntoView();
+// const second = document.getElementById('second-block');
+// second.scrollIntoView();
+// const three = document.getElementById('three-block');
+// three.scrollIntoView();
+// const four = document.getElementById('four-block');
+// four.scrollIntoView();
